de981881c6e3283a047f4cc896166fa04525ff62
楊布布 <yang.bubu@yangbubudeMacBook-Pro.local>
2026-04-05 00:10:22 +0800

    sync: 同步至1179張照片（修正images.json）

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ async function fetchImagesFromDrive() {
     var params = {
         q: "'" + DRIVE_FOLDER_ID + "' in parents and mimeType contains 'image/'",
         fields: 'files(id, name, mimeType)',
-        pageSize: 500,
+        pageSize: 1200,
         key: GOOGLE_DRIVE_API_KEY
     };
     
